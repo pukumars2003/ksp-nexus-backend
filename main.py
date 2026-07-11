467952fb2c7ad216262c1ce5e27a81bb6a95ea28
ajaycom <ajayeffigoglobal@gmail.com>
2026-07-11 16:41:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 # Setup CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], # For development
+    allow_origins=["https://ksp-frontend-kvmmfxzm.onslate.in", "http://localhost:3000"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
